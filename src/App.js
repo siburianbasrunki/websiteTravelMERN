c45f87cf804d001899de443fef77221c1e46e423
siburianbasrunki <basrunkisiburian@gmail.com>
2023-04-01 21:55:45 +0700

    navbarOKE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import 'assets/scss/style.scss'
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      oke
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
