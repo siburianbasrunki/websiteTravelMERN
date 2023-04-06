267b6dcfd58db521bb4f3a30f80ca1ad0674cd16
siburianbasrunki <basrunkisiburian@gmail.com>
2023-04-06 13:51:16 +0700

    categoriesOKE

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -9,7 +9,7 @@ import Button from "elements/Button";
 export default function Hero(props) {
   function showMostPicked() {
     window.scrollTo({
-      top: (props.refMostPicked.current.offSeTop = 30),
+      top: props.refMostPicked.current.offSetTop + 30,
       behavior: "smooth",
     });
   }
